1625750233ba1cefac541840660cfa2636074fb5
Scott Moore <scott@galois.com>
2022-09-02 16:21:41 -0400

    Make UAF POI consider uses of all allocation subregions
    Fixes #52.

--- tests/postgres/poi_analysis/use_after_free_test.py
+++ tests/postgres/poi_analysis/use_after_free_test.py
@@ -12,7 +12,7 @@ def test_poi_use_after_free_read(cpg_db_v2):
         for (free, usage, _, _) in compute_use_after_free(cpg.session, cpg)
     }
 
-    expected = {(20, 24)}
+    expected = {(20, 24), (20, 25)}
     assert results == expected
 
 
@@ -27,7 +27,7 @@ def test_poi_use_after_free_write(cpg_db_v2):
         for (free, usage, _, _) in compute_use_after_free(cpg.session, cpg)
     }
 
-    expected = {(23, 27)}
+    expected = {(23, 27), (23, 28)}
     assert results == expected
 
 
@@ -42,5 +42,20 @@ def test_poi_use_after_free_cpp(cpg_db_v2):
         for (free, usage, _, _) in compute_use_after_free(cpg.session, cpg)
     }
 
-    expected = {(17, 21)}
+    expected = {(17, 21), (17, 22)}
+    assert results == expected
+
+
+def test_poi_use_after_free_ntu(cpg_db_v2):
+    cpg = cpg_db_v2("ntu-uaf.c")
+
+    results = {
+        (
+            free.location["line"],
+            usage.location["line"],
+        )
+        for (free, usage, _, _) in compute_use_after_free(cpg.session, cpg)
+    }
+
+    expected = {(76, 95)}
     assert results == expected
