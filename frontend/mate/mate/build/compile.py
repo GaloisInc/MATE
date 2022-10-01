db20e201f9a804317cdbb530143ffdbe77472006
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 00:32:36 +0000

    Adding tarfile member sanitization to extractall()

--- frontend/mate/mate/build/compile.py
+++ frontend/mate/mate/build/compile.py
@@ -710,7 +710,26 @@ class _Compiler:
         try:
             # TODO(ww): Maybe support other kinds of tarballs/archives? Worth it?
             with tarfile.open(local_target, "r:gz") as tar:
-                tar.extractall(path=local_target_dir.name)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, path=local_target_dir.name)
         except tarfile.TarError as e:
             self._raise(
                 f"failed to open/extract tarball: {e}",
@@ -785,7 +804,26 @@ class _Compiler:
         local_target = artifact.persist_locally(suffix=".tar.gz")
         try:
             with tarfile.open(local_target, "r:gz") as tar:
-                tar.extractall(path=scratch_src)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, path=scratch_src)
         except tarfile.TarError as e:
             self._raise(
                 "failed to open/extract tarball",
